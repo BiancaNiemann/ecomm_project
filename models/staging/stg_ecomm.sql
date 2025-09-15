c242e149acd0aa3efd6664d67ec04232fbebfc16
BiancaNiemann <papyrollsollutions@gmail.com>
2025-09-15 14:39:02 +0200

    staging_ models_commit

--- models/staging/stg_ecomm.sql
+++ models/staging/stg_ecomm.sql
@@ -3,10 +3,9 @@ select
     customer_id,
     product_id,
     product_name,
-    md5(category) AS category_id,
     category,
     quantity AS quantity_sold,
-    unit_price,
+    CAST(unit_price AS numeric) AS unit_price,
     order_date,
     'online_001' AS store_id,
     'Online' AS store_location,
@@ -15,14 +14,5 @@ select
     age_group,
     gender AS customer_gender,
     payment_method,
-    CASE extract(dayofweek from order_date)
-        WHEN 1 THEN 'Monday'
-        WHEN 2 THEN 'Tuesday'
-        WHEN 3 THEN 'Wednesday'
-        WHEN 4 THEN 'Thursday'
-        WHEN 5 THEN 'Friday'
-        WHEN 6 THEN 'Saturday'
-        WHEN 7 THEN 'Sunday'
-    END AS weekday,
     order_status
 from {{ source('raw_ecomm', 'raw_ecomm') }}
--- models/staging/stg_wallmart.sql
+++ models/staging/stg_wallmart.sql
@@ -1,17 +1,16 @@
 select 
-    transaction_id AS order_id,
-    customer_id,
-    product_id,
+    CONCAT('ORDW',transaction_id) AS order_id,
+    CONCAT('CUSTW',customer_id) AS customer_id,
+    CONCAT('PRODW',product_id) AS product_id,
     product_name,
-    md5(category) AS category_id,
     category,
     quantity_sold,
-    unit_price,
-    transaction_date AS order_date,
-    store_id,
+    CAST(unit_price as numeric) AS unit_price,
+    DATE(transaction_date) AS order_date,
+    CONCAT('STORW',store_id) AS store_id,
     store_location,
     'USA' AS country,
-    supplier_id,
+    CONCAT('SUPPW',supplier_id) AS supplier_id,
     CASE
         WHEN customer_age < 19 THEN 'Teenagers'
         WHEN customer_age BETWEEN 19 AND 59 THEN 'Adults'
@@ -19,6 +18,5 @@ select
     END AS age_group,
     customer_gender,
     payment_method,
-    weekday,
     'In_Shop' AS order_status
 from {{ source('raw_wallmart', 'raw_wallmart')}}
